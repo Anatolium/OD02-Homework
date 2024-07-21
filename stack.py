f505c02b69b4d45ba770444df0dce3f1392687d9
Анатолий <lan2002@yandex.ru>
2024-07-21 03:57:31 +0300

    (update) stack.py

--- stack.py
+++ stack.py
@@ -6,12 +6,15 @@ class Stack:
         return self.items == []
 
     def push(self, item):
+        print(f"Добавлен элемент {item}")
         self.items.append(item)
 
     def pop(self):
+        print(f"Удалён элемент {self.items[-1]}")
         return self.items.pop()
 
     def peek(self):
+        print(f"Последний элемент в стеке: {self.items[-1]}")
         return self.items[-1]
 
 stack = Stack()
@@ -22,6 +25,6 @@ stack.push(2)
 stack.push(3)
 
 print(stack.is_empty())
-print("last element:", stack.peek())
-print("delete element:", stack.pop())
-print("last element:", stack.peek())
+stack.peek()
+stack.pop()
+stack.peek()
