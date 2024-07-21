7c3707f312b1cc276e11a96f8a36b655a7465f66
Анатолий <lan2002@yandex.ru>
2024-07-21 04:05:26 +0300

    (update) stack.py

--- stack.py
+++ stack.py
@@ -17,6 +17,13 @@ class Stack:
         print(f"Последний элемент в стеке: {self.items[-1]}")
         return self.items[-1]
 
+    def print_stack(self):
+        if self.is_empty():
+            print("Стек пуст")
+        else:
+            print("Текущий стек:", " -> ".join(map(str, reversed(self.items))))
+
+
 stack = Stack()
 print(stack.is_empty())
 
@@ -28,3 +35,7 @@ print(stack.is_empty())
 stack.peek()
 stack.pop()
 stack.peek()
+
+stack.push(4)
+stack.push(5)
+stack.print_stack()
